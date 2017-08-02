aa58286e7d20c1831c6aad4f1c9e7387d7602c94
Pierre-Yves Aillet <pyaillet@gmail.com>
2017-08-02 14:02:39 +0200

    Prepare script

---
+++ aws/prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt-get update
+sudo apt-get install golang-go
+
+cd /home/docker
+git clone http://github.com/linuxkit/linuxkit
+cd linuxkit
+make && sudo make install
+
+export AWS_REGION=eu-west-1
+aws configure
+
+# Only once
+aws iam create-role --role-name vmimport --assume-role-policy-document file://trust-policy.json
+
