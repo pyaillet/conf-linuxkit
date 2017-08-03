b7c5fcffabf6fef0e46b741e26cef45004127389
Pierre-Yves Aillet <pyaillet@gmail.com>
2017-08-03 22:52:34 +0200

    EC2 preparation

--- aws/prepare.sh
+++ aws/prepare.sh
@@ -15,3 +15,5 @@ aws configure
 # Only once
 aws iam create-role --role-name vmimport --assume-role-policy-document file://trust-policy.json
 
+# Only once
+aws iam put-role-policy --role-name vmimport --policy-name vmimport --policy-document file://role-policy.json
