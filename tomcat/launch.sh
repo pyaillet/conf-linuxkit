2c8a13c6fa18ac27fe96d5e623b0040e337927b7
Eric Briand <eric.briand@gmail.com>
2017-09-26 22:32:09 +0200

    renamed tomcat into tomcat-os

--- tomcat/launch.sh
+++ tomcat/launch.sh
@@ -3,5 +3,5 @@
 ./buildWar.sh
 docker build -t devfest-getty ./getty
 docker build -t devfest-tomcat-linuxkit .
-moby build tomcat.yml
-sudo linuxkit run --networking=vmnet tomcat
+moby build tomcat-os.yml
+sudo linuxkit run --networking=vmnet tomcat-os
