8f7819ffc2843ab2b66b385f4658d19cb7522ff3
Pierre-Yves Aillet <pyaillet@gmail.com>
2017-10-10 20:49:08 +0200

    Makefiles & linuxkit pkg build

--- tomcat/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./buildWar.sh
-docker build -t devfest-getty ./getty
-docker build -t devfest-tomcat-linuxkit .
-moby build tomcat-os.yml
-sudo linuxkit run --networking=vmnet tomcat-os
